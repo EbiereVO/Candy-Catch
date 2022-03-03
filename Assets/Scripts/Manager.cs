3371ce940a58701d25ba73dcad5f8bb34083881a
Ebiere Orubide <ebiereorubide@gmail.com>
2022-03-03 09:27:48 +0100

    create gameover panel

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Manager : MonoBehaviour
 {
 
     int score = 0;
-    bool GameOver = false;
+    bool gameOver = false;
+    int lives = 3;
     public static Manager instance;
+    public GameObject livesHolder;
+    public GameObject gameoverPanel;
+
+    public Text scoreText;
 
     void Awake()
     {
@@ -16,7 +22,34 @@ public class Manager : MonoBehaviour
    
     public void incrementScore()
     {
-        score++;
-        Debug.Log(score);
+        if (!gameOver)
+        {
+            score++;
+            scoreText.text = score.ToString();
+        }
+    }
+
+    public void DecreaseScore()
+    {
+        if (lives > 0)
+        {
+            lives--;
+            Debug.Log(lives);
+            livesHolder.transform.GetChild(lives).gameObject.SetActive(false);
+        }
+
+        if (lives <= 0)
+        {
+            gameOver = true;
+            GameOver();
+        }
+    }
+
+    public void GameOver()
+    {
+        Spawner.instance.stopSpawn();
+        GameObject.Find("Player").GetComponent<PlayerController>().canMove = false;
+        gameoverPanel.SetActive(true);
+        Debug.Log("GameOver()");
     }
 }
